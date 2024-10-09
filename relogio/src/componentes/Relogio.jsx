3e53356b9dddda7ad3adf505c579b95ba1ae0ff2
Bruna Borges <bruna.lborges.matias@gmail.com>
2024-10-09 20:51:37 -0300

    useEffect

--- relogio/src/componentes/Relogio.jsx
+++ relogio/src/componentes/Relogio.jsx
@@ -1,6 +1,19 @@
 import React, {useState, useEffect} from 'react'
 
 function Relogio() {
+
+    const[tempo, setTempo] = useState(new Date())
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setTempo(new Date())
+        }, 1000)
+
+        return () => {
+            clearInterval(intervalId)
+        }
+    },[])
+
     return(
         <>
             <div className="containerRelogio">
